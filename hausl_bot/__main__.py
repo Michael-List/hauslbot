338878ab07f2d315e690d51fbee69deb71ece702
Michael List <michael.list@stud.fh-rosenheim.de>
2019-04-14 12:47:53 +0200

    Swicht to telegram beta 12. First running bot

--- hausl_bot/__main__.py
+++ hausl_bot/__main__.py
@@ -1,4 +1,9 @@
+import logging
 
+from hausl_bot.telegram_handler import TelegramHandler
 
 if __name__ == '__main__':
-    pass
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    telegram = TelegramHandler()
