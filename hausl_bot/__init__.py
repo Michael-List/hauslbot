fc65a3fc21cc3e584075be3cfac743c0baf197b7
Michael List <michael.list@stud.fh-rosenheim.de>
2019-04-14 17:28:17 +0200

    Support dotenv if not running inside a docker container

--- hausl_bot/__init__.py
+++ hausl_bot/__init__.py
@@ -1,4 +1,13 @@
 import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Load local .env from root folder of the project if not in docker container
+if not os.getenv('RUNNING_INSIDE_DOCKER', False):
+    load_dotenv(verbose=True)
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path)
 
 bot_token = os.environ['BOT_TOKEN']
 warn_cell_id = os.environ['WARN_CELL_ID']
+monitored_devices = os.environ['MONITORED_DEVICES'].split(',')
